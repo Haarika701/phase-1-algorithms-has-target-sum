528b950a1ce5f927e5ea2aeb61d6c89d5f55a4ad
HaarikaR <hramadugu@gmail.com>
2021-10-14 15:06:38 -0700

    commit tree 7e8f03ec0ec731a54b636494d5de008c1b5a8c66

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let sumArray = []
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      let sum = array[i] + array[j]
+     // console.log(sumArray)
+      sumArray.push(sum)
+    }
+    
+    
+  }
+  match = sumArray.find((sum) => sum === target)
+      if (match) {
+        return true
+      } else {
+        return false
+      }
 }
 
 /* 
@@ -8,11 +24,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  
+//iterate array using for loop to check if they add up tp total  //does not have to accoount for the last element...just the seoncd last element of the arrray
+// iterate second elementloop to find the value that targets the sum   //inner loop starts with the second position of the array
+//check if array at index === target
+//if equal return true else false
 */
 
 /*
   Add written explanation of your solution here
+  
 */
+//find the pair of numbers the adds up to the given sum,in an array
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -21,14 +44,15 @@ if (require.main === module) {
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
   console.log("");
-
+   
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
+  console.log(" => ",hasTargetSum([2,7,11,15],9))
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
 }
 
 module.exports = hasTargetSum;
